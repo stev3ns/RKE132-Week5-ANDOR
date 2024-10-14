ec93a2aaf09b2a1c8af5dc5d2fb88b3db4cf5994
Steven S <stevensarap321@gmail.com>
2024-10-14 15:01:34 +0300

    add week5 code

---
+++ OROperator/Program.cs
@@ -0,0 +1,22 @@
+﻿//Math >= 90 OR Chemistry >= 90 OR Biology >= 90
+
+//Math && Chemistry >= 90 || Math && Biology >= 90 || Chemistry && Biology >= 90 
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 && chemistry >= 90) || (math >= 90 && biology >= 90) || (chemistry >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
